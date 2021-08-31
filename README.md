0d650fbdc6d3babb45b6ddef092037970a23869c
samdhare <sam.d.hare@gmail.com>
2021-08-31 14:59:54 +0100

    Commit version 1 of README.md and mm_script.js

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-#Mystery-Message
+# Mystery Message
+## Introduction
+This script generates a random Cluedo guess. Each guess consists in naming a suspect of a murder, the room where it took place, and the weapon used.
+## Characters
++ Miss Scarlet
++ Colonel Mustard
++ Mrs. White
++ Reverend Green
++ Mrs. Peacock
++ Professor Plum
+## Rooms
++ Kitchen
++ Ballroom
++ Conservatory
++ Dining Room
++ Cellar
++ Billiard Room
++ Library
++ Lounge
++ Hall
++ Study
+## Weapons
++ Revolver
++ Dagger
++ Lead Pipe
++ Rope
++ Candlestick
++ Wrench
--- mm_script.js
+++ mm_script.js
@@ -1,7 +1,21 @@
-var func = (a, b) => {
-    a = 1;
-    b = 2;
-    return a + b;
+//Array to list the characters
+const characters = ['Miss Scarlet', 'Colonel Mustard', 'Mrs. White', 'Reverend Green', 'Mrs. Peacock', 'Professor Plum']
+
+//Array to list the rooms
+const rooms = ['Kitchen', 'Ballroom', 'Conservatory', 'Dining Room', 'Cellar', 'Billiard Room', 'Library', 'Lounge', 'Hall', 'Study']
+
+//Array to list the weapons
+const weapons = ['Revolver', 'Dagger', 'Lead Pipe', 'Rope', 'Candlestick', 'Wrench']
+
+//Function selects a random element of an array
+var generateRandomComponent = (arr) => {
+    const randomNum = Math.floor(Math.random() * arr.length);
+    return arr[randomNum];
 };
 
-console.log(func());
+//Function uses generateRandomComponent to print a string of a Cluedo guess
+var generateRandomGuess = () => {
+    console.log(`It was ${generateRandomComponent(characters)} in the ${generateRandomComponent(rooms)} with the ${generateRandomComponent(weapons)}.`)
+};
+
+generateRandomGuess();
